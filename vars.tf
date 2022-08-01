8286fed0cc85cb46b9eb2519b79933fc1b03a995
rajat sharma <108734280+rajat-cloudtechner@users.noreply.github.com>
2022-08-01 11:35:32 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  }
+variable "main_vpc_cidr" {
+  }
+variable "public_subnets" {
+  }
+variable "private_subnets" {
+  }
+variable "public_subnet_region" {
+  }
+variable "private_subnet_region" {
+  }
+variable "aws_access_key" {
+  }
+variable "aws_secret_key" {
+  }
